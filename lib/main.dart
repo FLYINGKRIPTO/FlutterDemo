8bc3848fd94368ae040ba4d4d00d7de0105dcd4a
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2019-03-23 10:19:41 +0530

    Initial commit, appbar,icon implemented

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: 'Welcome to flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Flutter is cool"),
+          leading: new Icon(
+            Icons.cake
+          ),
+        ),
+        body: new Center(
+          child: new Text("Hello World"),
+        ),
+      )
+    );
+  }
+}
